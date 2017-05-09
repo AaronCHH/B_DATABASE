2c087b409cabd295325e3235a70c669e8c495db9
AaronCHH <aaronhsu219@gmail.com>
2017-05-09 21:00:19 +0800

    init commit

---
+++ The Definitive Guide to SQLite/SF_TDGS/ch02/test.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE test(id integer primary key, value text);
+INSERT INTO "test" VALUES(1,'eenie');
+INSERT INTO "test" VALUES(2,'meenie');
+INSERT INTO "test" VALUES(3,'miny');
+INSERT INTO "test" VALUES(4,'mo');
+CREATE TABLE test2(id integer primary key, value text);
+INSERT INTO "test2" VALUES(2,'meenie');
+INSERT INTO "test2" VALUES(3,'miny');
+INSERT INTO "test2" VALUES(4,'mo');
+CREATE INDEX test_idx on test (value);
+CREATE VIEW schema as select * from sqlite_master;
+COMMIT;
